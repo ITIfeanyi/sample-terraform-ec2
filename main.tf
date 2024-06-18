d0dac9c1a18530f2685e40e89983e0e61cf13759
Ifeanyi Igweh <igwehifeanyi02@gmail.com>
2024-06-18 15:24:39 +0100

    added varible

--- main.tf
+++ main.tf
@@ -1,16 +1,11 @@
 terraform {
   required_providers {
-    aws = {
+    cloud = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
 }
-# Configure the AWS Provider
-provider "aws" {
-  region = var.region
-}
-
 # Create a VPC
 resource "aws_instance" "myec2" {
   ami = var.ami
